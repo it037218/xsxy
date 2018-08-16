0acf9a75f093a6c527af9d18672a7a62afea2034
Ivan <769964834@qq.com>
2018-08-16 08:29:05 +0800

    update

---
+++ app/Models/Book.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    //
+    protected $table = 'book';
+    protected $fillable = [
+        'book_name',
+        'description',
+        'type_id',
+        'author',
+        'cover_img_url',
+        'price',
+        'source',
+    ];
+
+    public function cover_image(){
+        return $this->hasOne(UploadFile::class,'cover_image_id','id');
+    }
+    public function images(){
+        return $this->belongsToMany(UploadFile::class,'book_image','book_id','file_id');
+    }
+}
--- database/migrations/2018_08_10_022959_create_book_table.php
+++ database/migrations/2018_08_10_022959_create_book_table.php
@@ -19,7 +19,7 @@ class CreateBookTable extends Migration
             $table->string('description')->comment('描述');
             $table->unsignedInteger('type_id')->comment('类型');
             $table->string('author')->comment('作者');
-            $table->unsignedInteger('cover_img_url')->comment('封面图片');
+            $table->unsignedInteger('cover_image_id')->comment('封面图片');
             $table->float('price')->comment('图书原价');
             $table->string('source')->comment('图书来源');
             $table->softDeletes();
