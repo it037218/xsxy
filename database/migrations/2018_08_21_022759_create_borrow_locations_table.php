cfea12c97991188b17250942465a311f6cb5aa8e
Ivan <769964834@qq.com>
2018-08-21 10:55:59 +0800

    update

---
+++ database/migrations/2018_08_21_022759_create_borrow_locations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBorrowLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('borrow_locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('commodity_id')->comment('商品编号');
+            $table->string('source','20')->comment('来源：borrow/person');
+            $table->double('latitude',10,6);
+            $table->double('longitude',10,6);
+            $table->string('location_name',100)->comment('左边名称');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('borrow_locations');
+    }
+}
