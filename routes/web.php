0e5fdd357e908f6a2e735423537e6247d632d238
Ivan <769964834@qq.com>
2018-12-06 23:36:45 +0800

    add image upload

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::prefix('app')->group(function () {
         Route::get('show','TeacherController@show');
     });
     Route::group(['prefix'=>'system'],function (){
-        Route::get('upload','SystemController@store');
+        Route::post('upload','SystemController@store');
     });
 });
 
