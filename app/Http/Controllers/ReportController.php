12020bff05a18c46296770d07da496a676e88039
Ivan <769964834@qq.com>
2018-12-09 13:46:40 +0800

    update

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -80,12 +80,12 @@ class ReportController extends Controller
         $rst->status = !$rst->status;
         $result = $rst->save();
 
-        $rst1 = UserReportAgree::where('openid', $openid)->where('report_id',$reportId)->first();
-        if ($rst1->status) {
-            Report::find($reportId)->increment('likes');
-        } else {
-            Report::find($reportId)->decrement('likes');
-        }
+         UserReportAgree::where('openid', $openid)->where('report_id',$reportId)->first();
+//        if ($rst1->status) {
+//            Report::find($reportId)->increment('likes');
+//        } else {
+//            Report::find($reportId)->decrement('likes');
+//        }
         return ['success' => $result ? 1 : 0];
     }
 
