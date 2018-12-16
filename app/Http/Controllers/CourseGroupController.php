673c6e1229096834549a5a993ef3f379d46dab91
Ivan <769964834@qq.com>
2018-12-16 22:38:09 +0800

    update

--- app/Http/Controllers/CourseGroupController.php
+++ app/Http/Controllers/CourseGroupController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Course;
 use App\Models\CourseCharge;
 use App\Models\CourseGroup;
 use App\Models\CourseGroupMember;
@@ -30,7 +29,10 @@ class CourseGroupController extends Controller
             'left_num' => $leftNum
         ];
         $result = CourseGroup::create($data);
-        if ($result) {
+        $groupId = $result->id;
+        $rst = CourseGroupMember::create(['openid' => $openid, 'course_id' => $courseId, 'group_id' => $groupId, 'status' => 1]);
+        CourseGroup::find($groupId)->decrement('left_num');
+        if ($rst) {
             return ['success' => 1, 'group_id' => $result->id];
         } else {
             return ['success' => 0];
@@ -42,27 +44,35 @@ class CourseGroupController extends Controller
         $openid = $request->input('openid');
         $groupId = $request->input('group_id');
         $courseId = $request->input('course_id');
+        $type = $request->input('type');
         $groupDetail = CourseGroup::find($groupId);
         //判断拼团是否已满
         if ($groupDetail->left_num == 0) {
             return ['success' => 0, 'msg' => '拼团已满'];
         }
 
-        //判断是否已经参与拼团
-        $rst = CourseGroupMember::where('openid', $openid)
-            ->where('group_id', $groupId)
-            ->first();
+        if ($type == 1) {
+            //判断是否已经参与拼团
+            CourseGroupMember::where('openid', $openid)
+                ->where('group_id', $groupId)
+                ->first();
+            $rst = CourseGroupMember::firstOrCreate(['openid' => $openid, 'course_id' => $courseId, 'group_id' => $groupId, 'status' => 1]);
+            CourseGroup::find($groupId)->decrement('left_num');
 
-        if ($rst) {
-            return ['success' => 0, 'msg' => '已参团'];
+        } else {
+            $rst = CourseGroupMember::firstOrCreate(['openid' => $openid, 'course_id' => $courseId, 'group_id' => $groupId, 'status' => 0]);
         }
 
-        $rst = CourseGroupMember::create(['openid' => $openid, 'course_id' => $courseId, 'group_id' => $groupId]);
+
+//        if ($rst) {
+//            return ['success' => 0, 'msg' => '已参团'];
+//        }
+
+//        $rst = CourseGroupMember::create(['openid' => $openid, 'course_id' => $courseId, 'group_id' => $groupId]);
 
         //检查拼团是否已满
         if ($rst) {
-            CourseGroup::find($groupId)->decrement('left_num');
-            return ['success' => 1];
+            return ['success' => 1, 'msg' => '参团成功'];
         } else {
             return ['success' => 0, 'msg' => '参团失败'];
         }
